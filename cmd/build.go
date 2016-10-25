12bba65bc4a9d50f8a2f55295b880ec1c5a7ccf9
Matias Insaurralde <matias@insaurral.de>
2016-10-25 14:43:16 -0300

    Restore build command functionality.

--- cmd/build.go
+++ cmd/build.go
@@ -17,22 +17,24 @@ package cmd
 import (
 	"fmt"
 
+	bundle "github.com/TykTechnologies/tyk-cli/commands/bundle"
 	"github.com/spf13/cobra"
 )
+
+var buildOutput, key string
+var skipSigning bool
 
 // buildCmd represents the build command
 var buildCmd = &cobra.Command{
 	Use:   "build",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Builds a plugin bundle",
+	Long:  `This command will create a bundle, it will take a manifest file and its specified files as input.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		fmt.Println("build called")
+		var err error
+		err = bundle.Bundle("build", buildOutput, key, &skipSigning)
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
 	},
 }
 
@@ -43,7 +45,10 @@ func init() {
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// buildCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// buildCmd.PersistentFlags().String("output", "", "Output file")
+	buildCmd.PersistentFlags().StringVarP(&buildOutput, "output", "o", "", "Output file")
+	buildCmd.PersistentFlags().StringVarP(&key, "key", "k", "", "Key for bundle signature")
+	buildCmd.PersistentFlags().BoolP("skip-signing", "y", false, "Skip bundle signing")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
